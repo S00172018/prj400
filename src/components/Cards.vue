2ffa555e6b603e94a830a2ef02cad381dccd72c9
David Fox <davidfoxington97@gmail.com>
2020-02-05 10:35:07 +0000

    calendar component (in progress)

---
+++ src/components/Cards.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo"/>
+        </div>
+    </v-card-text>
+    <v-card-actions>
+    </v-card-actions>
+  </v-card>
+</template>
+
+
+<script>
+import TodoItem from './Item.vue';
+
+export default {
+    name:"Cards",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,19 @@
       <Header/>
     <h1>Calendar</h1>
     <Calendar/>
+    <Cards/>
   </div>
 </template>
 
 <script>
 import Calendar from '../components/Calendar'
+import Cards from '../components/Cards'
+
 export default {
 
   components: {
-    Calendar
+    Calendar,
+    Cards
   }
   
 }
