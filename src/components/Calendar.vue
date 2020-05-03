f34ace6484f246810fa2a66b6dbfbc5db156f7c3
David Fox <s00172018@mail.itsligo.ie>
2020-05-03 14:01:57 +0100

    Added create event to calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="demo-app">
-    <div id="external-events">
-      <p>
-        <strong>Draggable Events</strong>
-      </p>
-      <div class="fc-event">My Event 1</div>
-      <div class="fc-event">My Event 2</div>
-      <div class="fc-event">My Event 3</div>
-      <div class="fc-event">My Event 4</div>
-      <div class="fc-event">My Event 5</div>
-      <p>
-        <input type="checkbox" id="drop-remove" />
-        <label for="drop-remove">remove after drop</label>
-      </p>
-    </div>
-
     <div id="calendar-container">
       <div id="calendar"></div>
     </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,8 @@
 <template>
-  <div class="about">
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col>
-          <h1>Tasks</h1>
-          <b-list-group v-bind:key="task.id" v-for="task in tasks">
-            <draggable v-model="exampleList">
-              <b-list-group-item
-                class="droppable"
-                variant="secondary"
-                v-bind:tasks="task"
-              >
-                {{ task.title }}
-                <b-button
-                  class="float-right"
-                  pill
-                  variant="outline-danger"
-                  size="sm"
-                  v-on:click="deleteEvent(task.id)"
-                  >Delete</b-button
-                >
-              </b-list-group-item>
-            </draggable>
-          </b-list-group>
-        </b-col>
-        <b-col cols="9"><Calendar /></b-col>
-      </b-row>
-    </b-container>
+  <div class="container" align="center">
+    <h1>Calendar</h1>
+    <CreateTask />
+    <Calendar />
   </div>
 </template>
 
@@ -34,12 +10,12 @@
 import Calendar from "../components/Calendar";
 import { db } from "@/main";
 import firebase from "firebase";
-import draggable from "vuedraggable";
+import CreateTask from "../components/CreateTask";
 
 export default {
   components: {
     Calendar,
-    draggable,
+    CreateTask,
   },
 
   data: () => ({
@@ -67,3 +43,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.about {
+  text-align: center;
+  align-content: center;
+}
+</style>
